988979764ccb47e87d1100568ff1dfe5abe1be5f
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2017-09-09 08:04:38 -0300

    Traer artistas de una URL externa

--- api/artistas.js
+++ api/artistas.js
@@ -1,12 +1,15 @@
 import artistas from './json-artistas'
+const ARTISTS_URL =
+  'https://raw.githubusercontent.com/underscopeio/workshop-react-native/master/api/json-artistas.json?token=ABk15HtAZTqW3x0AjDYQhUe183wkDMWZks5ZvQNkwA%3D%3D'
 
 export const obtenerArtistas = () => {
-  // return fetch('URL').then(res => res.json())
-  return Promise.resolve(artistas).then(response =>
-    response.artists.items.map(artist => ({
-      name: artist.name,
-      key: artist.id,
-      image: artist.images[1] && artist.images[1].url,
-    }))
-  )
+  return fetch(ARTISTS_URL)
+    .then(res => res.json())
+    .then(response =>
+      response.artists.items.map(artist => ({
+        name: artist.name,
+        key: artist.id,
+        image: artist.images[1] && artist.images[1].url,
+      }))
+    )
 }
