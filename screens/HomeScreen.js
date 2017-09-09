baa0cbb6fc84399021e46ed9e0aad653d07eecdc
Gonzalo Aguirre <aguirre.gonzalo@gmail.com>
2017-09-08 22:56:30 -0300

    Crear listado de 500 artistas usando ScrollView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,17 +7,15 @@ const artista = {
   image: 'https://lh6.googleusercontent.com/eOcwBtI4_kBP2prhL0A5puNNUCNAlawW6guVHltl6L4=s865-no',
 }
 
+const artistas = new Array(500).fill(artista)
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Artista datos={artista} />
-      </View>
-    )
+    return <ScrollView style={styles.container}>{artistas.map(artista => <Artista datos={artista} />)}</ScrollView>
   }
 }
 
